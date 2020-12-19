a04da7dfba7f06bde04011d40b92ab043252e90b
zth <380041632@qq.com>
2020-12-19 13:36:43 +0800

    add TestAxios

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import DataView from '../views/DataView'
 import LinePage from '../views/LinePage'
+import TestAxios from '../views/TestAxios'
 
 Vue.use(VueRouter)
 
@@ -12,6 +13,9 @@ const routes = [
   }, {
     path: '/linepage',
     component: LinePage
+  }, {
+    path: '/testaxios',
+    component: TestAxios
   }
 ]
 
